1ffeecff641f278e83254d624e398225df63f2e0
Tobias Koltsch <tobias@koltsch.de>
2018-02-09 07:54:03 +0100

    Basic Spring Boot application for zuul server.

---
+++ infrastructure/zuul-server/src/main/java/org/nerdcoding/example/spring/cloud/zuul/ZuulServerApplication.java
@@ -0,0 +1,12 @@
+package org.nerdcoding.example.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
