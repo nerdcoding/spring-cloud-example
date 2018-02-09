ab7c3adb798a24ed230110dd76de8483c3dfe1cc
Tobias Koltsch <tobias@koltsch.de>
2018-02-09 10:35:27 +0100

    Explicitly configure Eureka client in properties.

--- microservices/ms-product/src/main/java/org/nerdcoding/example/spring/cloud/ms/product/ProductApplication.java
+++ microservices/ms-product/src/main/java/org/nerdcoding/example/spring/cloud/ms/product/ProductApplication.java
@@ -2,12 +2,8 @@ package org.nerdcoding.example.spring.cloud.ms.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableEurekaClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
