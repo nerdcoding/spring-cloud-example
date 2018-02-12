6d89ac23d5cadb741293f7fd09867844f93845b0
Tobias Koltsch <tobias@koltsch.de>
2018-02-12 13:07:39 +0100

    Enable required authentication for all REST endpoints.
    All incoming requests needs to be authenticated (see
    ResourceServerConfiguration). The requests have to contain a HTTP
    authentication header with an Bearer token. This token is validated by
    the OAuth2 authentication server at the `/auth/user` endpoint.

--- microservices/ms-product/src/main/java/org/nerdcoding/example/spring/cloud/ms/product/ProductApplication.java
+++ microservices/ms-product/src/main/java/org/nerdcoding/example/spring/cloud/ms/product/ProductApplication.java
@@ -2,8 +2,10 @@ package org.nerdcoding.example.spring.cloud.ms.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class ProductApplication {
 
 	public static void main(String[] args) {
---
+++ microservices/ms-product/src/main/java/org/nerdcoding/example/spring/cloud/ms/product/ResourceServerConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * ResourceServerConfiguration.java
+ *
+ * Copyright (c) 2018, Tobias Koltsch. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ */
+
+package org.nerdcoding.example.spring.cloud.ms.product;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        // all incoming requests need a valid access_token
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
